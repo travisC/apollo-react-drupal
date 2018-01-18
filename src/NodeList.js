d4f4f41d717dea3b7f7887b5697e1e11df1eac9d
Travis Cunningham <travis@travis-cunningham.com>
2018-01-18 09:25:16 -0800

    Update NodeList.js
    switched to nodeQuery

--- src/NodeList.js
+++ src/NodeList.js
@@ -4,15 +4,20 @@ import { graphql } from 'react-apollo';
 
 // The data prop, which is provided by the wrapper below contains,
 // a `loading` key while the query is in flight and posts when it is ready
-function NodeList({ data: { loading, nodes } }) {
+function NodeList({ data: { loading, nodeQuery } }) {
+  //console.log(nodeQuery)
   if (loading) {
     return <div>Loading</div>;
   } else {
     return (
-      <ul>
-        {nodes.map(node =>
-          <li key={node.entities.entityId}>
-            {node.entityLabel}
+      <ul className="node-list">
+        {nodeQuery.entities.map(node =>
+          <li key={node.entityId}>
+            ID: {node.entityId}<br /> 
+            Title: {node.entityLabel}<br />
+            Type: {node.__typename}<br />
+            Subtitle: {node.fieldSubtitle ? node.fieldSubtitle : ''}<br />
+            Owner: {node.entityOwner.name}
           </li>
         )}
       </ul>
@@ -22,14 +27,26 @@ function NodeList({ data: { loading, nodes } }) {
 
 // The `graphql` wrapper executes a GraphQL query and makes the results
 // available on the `data` prop of the wrapped component (PostList here)
+const fragments = gql`
+fragment fragUser on User {
+  name
+}
+`;
 export default graphql(gql`
 {
-  nodeQuery {
+  nodeQuery(filter:{status:true, langcode: "en"}) {
     entities {
-      entityId
       entityLabel
+      entityId
+      entityOwner {
+        ... fragUser
+      }
       __typename
+      ... on NodeGiftCardPage {
+        fieldSubtitle
+      }
     }
   }
 }
+${fragments}
 `)(NodeList);
